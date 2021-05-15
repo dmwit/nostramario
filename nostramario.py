24ff537c7b7b1ec815bbe01dde1fdce198a2f936
Daniel Wagner <me@dmwit.com>
2021-05-15 00:16:53 -0400

    get one template match to work

--- nostramario.py
+++ nostramario.py
@@ -1,11 +1,14 @@
 import cv2
+import math
 import numpy
 import sys
 import Xlib.display
 import Xlib.ext.composite
 import Xlib.X
 
-def find_fceux(dpy):
+def find_fceux(dpy=None):
+    if dpy is None: dpy = Xlib.display.Display()
+
     for i in range(dpy.screen_count()):
         windows = [dpy.screen(i).root]
         j = 0
@@ -13,7 +16,10 @@ def find_fceux(dpy):
             windows = windows + windows[j].query_tree().children
             j = j+1
     fceuxs = [w for w in windows if w.get_wm_class() == ('fceux', 'fceux')]
-    if len(fceuxs) == 1: return fceuxs[0]
+
+    if len(fceuxs) == 1:
+        Xlib.ext.composite.redirect_window(fceuxs[0], Xlib.ext.composite.RedirectAutomatic)
+        return fceuxs[0]
     if len(fceuxs) == 0:
         print('Could not find an fceux window, exiting')
         sys.exit(1)
@@ -32,7 +38,19 @@ def screenshot_window(win):
     return numpy.frombuffer(img, dtype=numpy.uint8).reshape(h, w, 4)[:, :, :3]
 
 if __name__ == "__main__":
-    dpy = Xlib.display.Display()
-    fceux = find_fceux(dpy)
-    Xlib.ext.composite.redirect_window(fceux, Xlib.ext.composite.RedirectAutomatic)
-    cv2.imwrite('fceux.png', screenshot_window(fceux))
+    fceux = find_fceux()
+
+    w, h, _ = screenshot_window(fceux).shape
+    scale = math.sqrt(float(w*h)/(256.0*244.0))
+
+    template = cv2.imread("templates/progress-box.png", cv2.IMREAD_UNCHANGED)
+    w, h, _ = [round(dim*scale) for dim in template.shape]
+    template = cv2.resize(template, (h, w))
+    template_colors = template[:,:,:3]
+    template_mask = numpy.array(template[:,:,3])
+
+    while cv2.waitKey(1) == -1:
+        img = numpy.array(screenshot_window(fceux))
+        matches = cv2.matchTemplate(img, template_colors, cv2.TM_CCORR_NORMED, template_mask)
+        r, c = cv2.minMaxLoc(matches)[3]
+        cv2.imshow('progress box', cv2.rectangle(img, (r, c), (r+h, c+w), (255, 255, 0), 2))
