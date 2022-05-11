90b24b8600113ace0edbb5ea8e6bbbf7a1490f41
Daniel Wagner <me@dmwit.com>
2022-05-11 14:05:30 -0400

    fix positioning bug in noisy scaler

--- nightmario.py
+++ nightmario.py
@@ -1106,8 +1106,8 @@ SCALING_METHODS = [cv2.INTER_NEAREST, cv2.INTER_LINEAR, cv2.INTER_CUBIC, cv2.INT
 def noisy_scale(image):
     fx = random.normalvariate(3, 0.1)
     fy = random.normalvariate(3, 0.1)
-    dx = random.normalvariate(0, 1) + (fx-3)*image.shape[1]/2
-    dy = random.normalvariate(0, 1) + (fy-3)*image.shape[0]/2
+    dx = random.normalvariate(0, 1) + (3-fx)*image.shape[1]/2
+    dy = random.normalvariate(0, 1) + (3-fy)*image.shape[0]/2
 
     if not random.randrange(100):
         background = numpy.ones((image.shape[0]*3, image.shape[1]*3, 3), dtype=numpy.uint8) \
