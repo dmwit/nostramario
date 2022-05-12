5482b16eb70894ad6aa3d0ad833295e19d602f79
Daniel Wagner <me@dmwit.com>
2022-05-12 00:40:40 -0400

    generate training examples in parallel with training

--- nightmario.py
+++ nightmario.py
@@ -3,6 +3,7 @@ import dataclasses
 import importlib
 import itertools
 import math
+import multiprocessing
 import numpy
 import numpy.random
 import random
@@ -12,7 +13,7 @@ import tensorboardX as tx
 import torch
 
 from dataclasses import dataclass
-from os import path, walk
+from os import path, urandom, walk
 from torch import nn
 from typing import Dict, Generic, List, Optional, Set, Tuple, TypeVar, Union
 
@@ -872,6 +873,8 @@ class TrainingExample:
     # classification problem in an arbitrary collection of indices
     onehot_indices: List[int]
 
+    def to(self, dev): return TrainingExample(self.clean_image, self.filtered_image, self.classification.to(device=dev), self.onehot_ranges, self.onehot_indices)
+
 # Create hybrid training examples. Linearly mix a collection of images and
 # their classifications. Typically you want to supply a collection with just
 # two examples. This can aid in convergence and robustness of the network.
@@ -1222,6 +1225,18 @@ class Classifier(nn.Module):
             self.fully_connected = nn.Linear(unstructured_features.shape[1], self.out_size, device=self.device)
         return self.fully_connected(unstructured_features)
 
+def generate_examples_forever(q, scenes):
+    random.seed(urandom(2048))
+    cache = TemplateCache()
+    # The pytorch documentation on multiprocessing has all kinds of caveats and
+    # tricks that you need to pay attention to if you want to share GPU tensors
+    # between processes. Let's just keep it simple and toss everything on the
+    # CPU, accept the communication cost, and copy to the GPU in the consumer
+    # process. This way it's hard to go wrong; and it seems unlikely that the
+    # communication costs are a serious consideration for what we're doing.
+    dev = torch.device('cpu')
+    while True: q.put(scenes.select().render(cache, dev))
+
 def xe_loss_single(probs, target):
     return nn.functional.cross_entropy(torch.unsqueeze(probs, 0), torch.unsqueeze(target, 0), label_smoothing=0.01)
 
@@ -1246,6 +1261,13 @@ def log_params(n):
 with open('layouts/layered.txt') as f:
     _, _, t = parse_scene_tree(f)
 scenes = Scenes(t)
+# We can't increase (or decrease) the max queue size later if the number of
+# training examples per batch changes. So just pick some bound that's a bit
+# bigger than the current number of examples and hope it's big enough.
+q = multiprocessing.Queue(16*max(1, params.EXAMPLES_PER_MIX*params.MIXES_PER_BATCH))
+# cpu_count-1 to leave a CPU for non-nightmario uses lmao
+for _ in range(max(1, multiprocessing.cpu_count() - 1)):
+    multiprocessing.Process(target=generate_examples_forever, args=(q, scenes), daemon=True).start()
 
 dev = torch.device('cuda')
 cache = TemplateCache()
@@ -1271,6 +1293,9 @@ opt = torch.optim.SGD(c.parameters(), params.LEARNING_RATE, momentum=params.MOME
 #     if cv2.waitKey(100) == 113:
 #         break
 
+# This bit is done single-threaded. In the long run, the multi-threaded
+# approach produces the right distribution, but in the short term it may
+# briefly be biased towards scenes that render more quickly.
 test = []
 for _ in range(params.TEST_EXAMPLES):
     test.append(scenes.select().render(cache, dev))
@@ -1288,7 +1313,7 @@ while(True):
 
     train = []
     for _ in range(params.EXAMPLES_PER_MIX*params.MIXES_PER_BATCH):
-        train.append(scenes.select().render(cache, dev))
+        train.append(q.get().to(dev))
     train = [merge_examples(train[i::params.MIXES_PER_BATCH]) for i in range(params.MIXES_PER_BATCH)]
     train_tensor = torch.tensor(numpy.array([x.filtered_image for x in train]), device=dev)
 
